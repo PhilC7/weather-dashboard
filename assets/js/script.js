ced09a59b1329a022e951710aca2c405cd45077c
Phil Cooper <philjcooper@outlook.com>
2024-01-27 10:25:18 +0000

    checked api key is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,29 @@ $(document).ready(function () {
     * GLOBAL VARIABLES
     ********************/
     var apiKey = "7a91cf58326cd6da40050bda09317cd6"
-    var cityName = $("#search-input");
+    // var cityName = $("#search-input");
+    var cityName = "London";
     var history = $("#history");
     var today = $("#today");
     var forecast = $("#forecast");
+    var locations = [];
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey;
 
-    console.log(forecast);
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
 
+        });
 
 });
+
+
+// HOW TO ACCES REQUIRED INFO
+// console.log(data.name);
+// console.log(Math.floor(data.main.temp - 273.15) + "˚C");
+// console.log(data.wind.speed);
+// console.log(data.weather[0].icon);
